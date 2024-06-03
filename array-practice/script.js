44d4e434a641e9c54ed4c25699d30a7d0d89f787
Rafi-Rafique <160730669+Rafi-Rafique@users.noreply.github.com>
2024-06-03 23:41:20 +0500

    Add files via upload

---
+++ array-practice/script.js
@@ -0,0 +1,34 @@
+// .......................variable-copy
+
+// let sub = "eng";
+// let copySub = sub;
+// console.log(sub);
+// console.log(copySub);
+// sub ="urdu"
+// console.log(sub);
+// console.log(copySub);
+
+// .............array-copyS
+
+// let veg =["onion","chilli","lemon","potato","peas","ginger","garlic","mint"];
+// console.log(veg);
+// console.log(copyVeg);
+// veg[1] = "carrot";
+// console.log(veg);
+// console.log(copyVeg);
+
+
+// ..........................deep copy
+// let veg =["onion","chilli","lemon","potato","peas","ginger","garlic","mint"];
+// let copySlice = veg.slice(1,6);
+// console.log(veg);
+// console.log(copySlice);
+
+
+// .....................array-methods
+// let veg =["onion","chilli","lemon","potato","peas","ginger","garlic","mint"];
+// veg.pop();
+// veg.shift();
+// veg.shift();
+// veg.splice(2,0,"tomato")
+// console.log(veg);
